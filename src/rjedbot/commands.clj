09f40eca5f949e737735a041c9c998c89eeaaad4
Luciano Laratelli <luciano@laratelli.com>
2021-01-15 04:06:42 +0000

    hacking on commands making progress on embeds
    currently broken in this state; currently not sending responses back to
    discord for any commands

--- src/rjedbot/commands.clj
+++ src/rjedbot/commands.clj
@@ -1,6 +1,8 @@
 (ns rjedbot.commands
   (:require [jsonista.core :as j]
-            [rjedbot.reddit :as reddit]))
+            [rjedbot.reddit :as reddit]
+            [clojure.pprint :as pp]
+            [clojure.string :as s]))
 
 (defn make-string-response
   [string]
@@ -8,9 +10,71 @@
    :headers {"Content-Type" "application/json"}
    :body (j/write-value-as-string {:type 4 :data {:content string}})})
 
+(defn make-embed
+  [url-string]
+  {:image {:url url-string}})
+
+(defn make-embed-from-urls
+  [url-list]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (j/write-value-as-string {:type 4
+                                   :data {:embeds
+                                          (into []
+                                                (map make-embed url-list))}})})
+
+(defn get-value
+  [m i]
+  (get-in m [i "value"]))
+
+(defn post-handler
+  "Handle post URLs according to their types."
+  ;;depending on the type of content, some posts must be embedded while other
+  ;;must be sent as raw links, yet others can not be sent at all. The
+  ;;restriction here is that certain content types do not view properly in
+  ;;discord. gifs won't play in embeds, for example. From simple
+  ;;experimentation, I've determined the following:
+  ;;no extension, gifv, gif => raw link (sent as follow-up messages after embed is sent)
+  ;;allowed extensions (png, jpg, jpeg) => embed (all in the same response)
+  ;;mp4 => skip (maybe a message in the response like "skipped n invalid files")
+  [posts]
+  (let [embed-extensions #{"png" "jpg" "jpeg"}
+        raw-msg-extensions #{"" "gifv" "gif"}
+        skipped-extensions #{"mp4"}
+        labelled-posts (map (fn [post]
+                              (let [extension
+                                    (last (s/split (apply str (take-last 5 post)) #"\."))]
+                                (cond
+                                  (contains? skipped-extensions extension) {:skip post}
+                                  (contains? embed-extensions extension) {:embed post}
+                                  :default {:raw post})))
+                            posts)
+        skipped (count (filter #(:skip %) labelled-posts))
+        to-embed (filter #(:embed % labelled-posts))
+        to-raw (filter #(:raw % labelled-posts))]
+    (make-embed-from-urls (map #(:embed %) (filter #(:embed %) labelled-posts)))))
+
+(def post-max 6)
+
+(defn valid-post-count? [pcount]
+  (<= 1 pcount post-max))
+
 (defn command-handler
   [body]
-  (case (get-in body ["data" "name"])
-    "lofi" (make-string-response "p!play https://www.youtube.com/watch?v=5qap5aO4i9A")
-    "blep" (make-string-response "hello")
-    "post" (make-string-response (first (reddit/get-posts (get-in body ["data" "options" 0 "value"]))))))
+  (let [data (get body "data")
+        options (get data "options")
+        command-name (get data "name")
+        subreddit (get-value options 0)
+        post-type (keyword (get-value options 1))
+        post-time-scope (keyword (get-value options 2))
+        post-count (get-value options 3)]
+    (pp/pprint body)
+    (println "hello")
+    (if (or (valid-post-count? count) (nil? post-count))
+      (case command-name
+        "lofi" (make-string-response "p!play https://www.youtube.com/watch?v=5qap5aO4i9A")
+        "post" (post-handler (reddit/get-posts subreddit))
+        "post-from" (post-handler (reddit/get-posts subreddit post-type))
+        "post-from-time" (post-handler (reddit/get-posts subreddit post-type post-time-scope))
+        "posts" (post-handler (reddit/get-posts subreddit post-type post-time-scope post-count)))
+      (make-string-response (conj "you asked for too many posts. max is " post-max)))))
