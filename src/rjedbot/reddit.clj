790f26391d02af8d2578f7a05a36a3f7d3dbb728
Luciano Laratelli <luciano@laratelli.com>
2021-01-22 21:21:21 +0000

    throw if no posts are returned from creddit

--- src/rjedbot/reddit.clj
+++ src/rjedbot/reddit.clj
@@ -29,17 +29,17 @@
   ([subreddit post-type time]
    (get-posts subreddit post-type time 1))
   ([subreddit post-type time amount-posts-requested]
-   (cond
-     (> amount-posts-requested 10) (throw (Exception. "Can't ask for more than 10 posts at a time."))
-     (< amount-posts-requested 1) (throw (Exception. "Can't ask for less than 1 post."))
-     (not (contains? creddit-times time)) (throw (Exception. "You asked for an invalid time. Must be any one of :hour, :day, :week, :month, :year, :all."))
-     :else (let
-            [posts (map
-                    #(:url %)
-                    (try
-                      ((get creddit-funcs post-type) creddit-client subreddit amount-posts-requested time)
-                      (catch Exception e
-                        (println "Exception in redbot.reddit/get-posts: " (.getMessage e)))))]
+   (let [posts (cond
+                 (> amount-posts-requested 10) (throw (Exception. "Can't ask for more than 10 posts at a time."))
+                 (< amount-posts-requested 1) (throw (Exception. "Can't ask for less than 1 post."))
+                 (not (contains? creddit-times time)) (throw (Exception. "You asked for an invalid time. Must be any one of :hour, :day, :week, :month, :year, :all."))
+                 :else (let
+                        [posts (map
+                                #(:url %)
+                                (try
+                                  ((get creddit-funcs post-type) creddit-client subreddit amount-posts-requested time)
+                                  (catch Exception e
+                                    (println "Exception in redbot.reddit/get-posts: " (.getMessage e)))))]
              ;; the creddit API returns 1 + amount-posts-requested if a
              ;; subreddit has a pinned post. I *think* this is because of a bug
              ;; in the reddit API, where pinned posts have a `false` :pinned
@@ -47,6 +47,9 @@
              ;; documented anywhere on creddit's github page, so who knows if
              ;; it's true or just a coincidence, but it is behavior I have
              ;; observed.
-             (if (> (count posts) amount-posts-requested)
-               (rest posts)
-               posts)))))
+                         (if (> (count posts) amount-posts-requested)
+                           (rest posts)
+                           posts)))]
+     (if (empty? posts)
+       (throw "No posts")
+       posts))))
