7008127e47579f8729d5af6da50f8f754fde134d
Luciano Laratelli <luciano@laratelli.com>
2021-01-22 14:30:47 +0000

    refactor utils, move them into util.clj, comments

--- src/rjedbot/util.clj
+++ src/rjedbot/util.clj
@@ -9,3 +9,26 @@
   (with-open [f (io/writer filename)]
     (binding [*out* f]
       (pr data))))
+
+(defn contains-key?
+  "Returns if map m contains key k."
+  [m k]
+  (if (get m k)
+    true
+    false))
+
+(defn get-ith-value-with-key
+  "Return the value for the key k in the ith map in a vec of maps."
+  ;; When m is
+  ;; [{"name" "subreddit", "value" "cats"} {"name" "section", "value" "top"}],
+  ;; calling this function with m, 0, and "value" returns "cats"
+  [m i k]
+  (get-in m [i k]))
+
+(defn get-inner-values-matching-key
+  "Get any values that match k from s, a list of maps."
+  ;; When s is
+  ;; ({:a 'foo'} {:b 'bar'} {:c 'baz'} {:a 'quux'})
+  ;; and we call this function with :a, we return 'foo' and 'quux'
+  [s k]
+  (map k (filter #(contains-key? % k) s)))
