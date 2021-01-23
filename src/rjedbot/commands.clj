f7412322b32ddde9744fa848f5af45d737af5e2d
Luciano Laratelli <luciano@laratelli.com>
2021-01-23 17:32:29 +0000

    Fix bug where POSTs aren't sent in the right order
    thanks elijah!

--- src/rjedbot/commands.clj
+++ src/rjedbot/commands.clj
@@ -1,8 +1,10 @@
 (ns rjedbot.commands
-  (:require [clojure.java.io :as io]
+  (:require [clojure.core.async :as a]
+            [clojure.java.io :as io]
             [clojure.pprint :as pp]
             [clojure.string :as s]
             [jsonista.core :as j]
+            [org.httpkit.server :as h]
             [rjedbot.config :as config]
             [rjedbot.reddit :as reddit]
             [rjedbot.responses :as responses]
@@ -40,52 +42,54 @@
         skipped (count (u/get-inner-values-matching-key labelled-posts :skip))
         embeddable (u/get-inner-values-matching-key labelled-posts :embed)
         to-raw (u/get-inner-values-matching-key labelled-posts :raw)]
-    (when (> skipped 0) (responses/POST-string token (str "Skipped " skipped " of " (count posts) " requested posts due to invalid format.")))
+    (when (> skipped 0)
+      (responses/POST-string token (str "Skipped " skipped " of " (count posts) " requested posts due to invalid format.")))
     (when (> (count embeddable) 0) (responses/POST-embed token embeddable))
     (when (> (count to-raw) 0)
       (for [r to-raw]
-        (responses/POST-single-URL token r)))
-
-    ;; {:status 200
-    ;;  :headers {"Content-Type" "application/json"}
-    ;;  :body (j/write-value-as-string {:type 4} :data {:content ""})}
-    ))
+        (responses/POST-single-URL token r)))))
 
 (defn valid-post-count?
   "Do we have a valid amount of posts?"
   ;; Allowing nil values as true because most of the commands do not actually
-  ;; include a post count -- only the /posts command does.
-  [c guild]
+  ;; include a post count -- only the /posts command does. [c guild]
   (if (nil? c)
     true
     (<= 1 c (:max-posts (get @config/config guild)))))
 
 (defn command-handler
   "Determine what response is made to incoming commands."
-  [body]
-  (let [data (get body "data")
-        options (get data "options")
-        command-name (get data "name")
-        token (get body "token")
-        guild (get body "guild_id")
-        subreddit (u/get-value-from-ith-map options 0 "value")
-        post-type (keyword (u/get-value-from-ith-map options 1 "value"))
-        post-time-scope (keyword (u/get-value-from-ith-map options 2 "value"))
-        post-count (u/get-value-from-ith-map options 3 "value")]
+  [body channel]
+  (h/send! channel
+           {:status 200
+            :headers {"Content-Type" "application/json"}
+            :body (j/write-value-as-string {:type 5})})
+  ;; http-kit appears to not be properly flushing the channel until we return
+  ;; here. Wrapping the `let` in an async thread resolves this issue.
+  (a/thread
+    (let [data (get body "data")
+          options (get data "options")
+          command-name (get data "name")
+          token (get body "token")
+          guild (get body "guild_id")
+          subreddit (u/get-value-from-ith-map options 0 "value")
+          post-type (keyword (u/get-value-from-ith-map options 1 "value"))
+          post-time-scope (keyword (u/get-value-from-ith-map options 2 "value"))
+          post-count (u/get-value-from-ith-map options 3 "value")]
 
-    (when (not (config/known-guild? guild))
-      (do
-        (reset! config/config (assoc @config/config guild config/default-guild-data))
-        (u/write-to-resource "config.edn" (str @config/config))))
+      (when (not (config/known-guild? guild))
+        (do
+          (reset! config/config (assoc @config/config guild config/default-guild-data))
+          (u/write-to-resource "config.edn" (str @config/config))))
 
-    (println (str "serving request for subreddit " subreddit " at " (new java.util.Date)))
-    (if (valid-post-count? post-count guild)
-      (try (case command-name
-             "lofi" (responses/POST-string token "p!play https://www.youtube.com/watch?v=5qap5aO4i9A")
-             "post" (post-handler (reddit/get-posts subreddit) token)
-             "post-from" (post-handler (reddit/get-posts subreddit post-type) token)
-             "post-from-time" (post-handler (reddit/get-posts subreddit post-type post-time-scope) token)
-             "posts" (post-handler (reddit/get-posts subreddit post-type post-time-scope post-count) token)
+      (println (str "serving request for subreddit " subreddit " at " (new java.util.Date)))
+      (if (valid-post-count? post-count guild)
+        (try (case command-name
+               "lofi" (responses/POST-string token "p!play https://www.youtube.com/watch?v=5qap5aO4i9A")
+               "post" (post-handler (reddit/get-posts subreddit) token)
+               "post-from" (post-handler (reddit/get-posts subreddit post-type) token)
+               "post-from-time" (post-handler (reddit/get-posts subreddit post-type post-time-scope) token)
+               "posts" (post-handler (reddit/get-posts subreddit post-type post-time-scope post-count) token)
              ;; "update-max" (do
              ;;           ;;"subreddit" used loosely here -- for most commands, the
              ;;           ;;value of the 0th map will be a subreddit. Here
@@ -93,8 +97,8 @@
              ;;           ;;owner wants to allow at a time.
              ;;                (swap! max-posts subreddit)
              ;;                (u/write-edn {:max-posts @max-posts} "config.edn"))
-             )
-           (catch Exception e
-             (responses/POST-string token (str "You asked for subreddit " subreddit ", which doesn't seem to exist."))))
+               )
+             (catch Exception e
+               (responses/POST-string token (str "You asked for subreddit " subreddit ", which doesn't seem to exist."))))
 
-      (responses/POST-string token (str "you asked for too many posts. max is " (:max-posts (get @config/config guild)))))))
+        (responses/POST-string token (str "you asked for too many posts. max is " (:max-posts (get @config/config guild))))))))
--- src/rjedbot/server.clj
+++ src/rjedbot/server.clj
@@ -13,22 +13,25 @@
 (def server (atom nil))
 
 (defn app
-  [thing]
-  (let [body-str (slurp (:body thing))
+  [request]
+  (let [body-str (slurp (:body request))
         body (j/read-value body-str)
-        timestamp (get (:headers thing) "x-signature-timestamp")
-        signature (cutil/unhexify (get (:headers thing) "x-signature-ed25519"))
+        timestamp (get (:headers request) "x-signature-timestamp")
+        signature (cutil/unhexify (get (:headers request) "x-signature-ed25519"))
         to-verify (str timestamp body-str)]
-    (try
-      (sign/verify signature to-verify discord-pubkey)
-      (case (get body "type")
-        1 {:status 200
-           :headers {"Content-Type" "application/json"}
-           :body (j/write-value-as-string {:type 1})}
-        2 (commands/command-handler body))
-      (catch RuntimeException e
-        {:status 401
-         :body "invalid request signature"}))))
+    (h/with-channel request channel
+      (try
+        (sign/verify signature to-verify discord-pubkey)
+        (case (get body "type")
+          1 (h/send! channel
+                     {:status 200
+                      :headers {"Content-Type" "application/json"}
+                      :body (j/write-value-as-string {:type 1})})
+          2 (commands/command-handler body channel))
+        (catch RuntimeException e
+          (h/send! channel
+                   {:status 401
+                    :body "invalid request signature"}))))))
 
 (defn start []
   (reset! server (h/run-server #'app {:port 44227}))
