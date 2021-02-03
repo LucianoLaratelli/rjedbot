10323035e845dce414d0df2d6c80fee1419e9fca
Luciano Laratelli <luciano@laratelli.com>
2021-02-03 13:49:29 -0500

    refactor responses, add logging

--- src/rjedbot/responses.clj
+++ src/rjedbot/responses.clj
@@ -1,37 +1,59 @@
 (ns rjedbot.responses
-  (:require  [ajax.core :refer [POST]]
+  (:require  [ajax.core :refer [POST GET]]
+             [cprop.core :refer [load-config]]
              [clojure.pprint :as pp]
-             [cprop.core :refer [load-config]]))
+             [rjedbot.log :refer [log]]
+             [clojure.core.async :as a]))
 
 (def application-id (:app-id (load-config :resource "discord-credentials.edn")))
+
+(def discord-webhook-url
+  (str "https://discord.com/api/v8/webhooks/" application-id "/"))
+
+(defn POST-string
+  [message-token string]
+  (log (str "POSTing single string using token " message-token " at " (new java.util.Date)))
+  (let
+   [url (str discord-webhook-url message-token)]
+    (POST url
+      {:format :json
+       :params {:content string}
+       :handler pp/pprint
+       :error-handler pp/pprint})))
+
+(defn POST-single-URL
+  [message-token url]
+  (log (str "POSTing single URL using token " message-token " at " (new java.util.Date)))
+  (let
+   [discord-url (str discord-webhook-url message-token)]
+    (POST discord-url
+      {:format :json
+       :params {:content url}
+       :handler pp/pprint
+       :error-handler pp/pprint})))
+
 (defn make-embed
   [url-string]
   {:image {:url url-string}})
 
-(defn POST-string
-  [message-token string]
-  (let
-   [url (str "https://discord.com/api/v8/webhooks/" application-id "/" message-token)]
-    (POST url
-      {:format :json
-       :params {:content string}
-       :handler pp/pprint})))
-
-(defn POST-single-URL
-  [message-token url]
-  (let
-   [discord-url (str "https://discord.com/api/v8/webhooks/" application-id "/" message-token)]
-    (POST discord-url
-      {:format :json
-       :params {:content url}
-       :handler pp/pprint})))
-
 (defn POST-embed
   [message-token urls]
+  (log (str "POSTing embed using token " message-token " at " (new java.util.Date)))
   (let
-   [url (str "https://discord.com/api/v8/webhooks/" application-id "/" message-token)]
+   [url (str discord-webhook-url message-token)]
     (POST url
       {:format :json
        :params {:embeds (into []
                               (map make-embed urls))}
-       :handler pp/pprint})))
+       :handler pp/pprint
+       :error-handler pp/pprint})))
+
+(defn GET-guild-owner
+  [guild-id]
+  (let
+   [url (str "https://discord.com/api/v8/guilds/" guild-id)]
+    (GET url
+      {:headers
+       {"authorization"
+        "Bot Nzk5MTIxOTU1NjQ2MjEwMDc4.X_--UQ.62jv5RML4QZxZ4DFLHczza5PRow"}})))
+
