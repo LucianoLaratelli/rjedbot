9abb0827f5536528beaee1fc02d5575553f5196c
Luciano Laratelli <luciano@laratelli.com>
2021-02-03 13:50:05 -0500

    clean up get-posts

--- src/rjedbot/reddit.clj
+++ src/rjedbot/reddit.clj
@@ -29,27 +29,26 @@
   ([subreddit post-type time]
    (get-posts subreddit post-type time 1))
   ([subreddit post-type time amount-posts-requested]
-   (let [posts (cond
-                 (> amount-posts-requested 10) (throw (Exception. "Can't ask for more than 10 posts at a time."))
-                 (< amount-posts-requested 1) (throw (Exception. "Can't ask for less than 1 post."))
-                 (not (contains? creddit-times time)) (throw (Exception. "You asked for an invalid time. Must be any one of :hour, :day, :week, :month, :year, :all."))
-                 :else (let
-                        [posts (map
-                                #(:url %)
-                                (try
-                                  ((get creddit-funcs post-type) creddit-client subreddit amount-posts-requested time)
-                                  (catch Exception e
-                                    (println "Exception in redbot.reddit/get-posts: " (.getMessage e)))))]
-             ;; the creddit API returns 1 + amount-posts-requested if a
-             ;; subreddit has a pinned post. I *think* this is because of a bug
-             ;; in the reddit API, where pinned posts have a `false` :pinned
-             ;; field, and this is a workaround. This isn't explicitly
-             ;; documented anywhere on creddit's github page, so who knows if
-             ;; it's true or just a coincidence, but it is behavior I have
-             ;; observed.
-                         (if (> (count posts) amount-posts-requested)
-                           (rest posts)
-                           posts)))]
-     (if (empty? posts)
-       (throw "No posts")
-       posts))))
+   (cond
+     (> amount-posts-requested 10)
+     (throw (Exception. "Can't ask for more than 10 posts at a time."))
+     (< amount-posts-requested 1)
+     (throw (Exception. "Can't ask for less than 1 post."))
+     (not (contains? creddit-times time))
+     (throw (Exception. "You asked for an invalid time. Must be any one of :hour, :day, :week, :month, :year, :all.")))
+   (let [posts (map #(:url %)
+                    (try
+                      ((get creddit-funcs post-type) creddit-client subreddit amount-posts-requested time)
+                      (catch Exception e
+                        (println "Exception in rjedbot.reddit/get-posts: " (.getMessage e)))))
+         retrieved (count posts)]
+     (cond
+       (empty? posts) (throw "No posts retrieved.")
+       (> retrieved amount-posts-requested) (drop (- retrieved amount-posts-requested) posts)
+       :else posts))))
+       ;; if a subreddit has pinned posts, creddit returns all of the pinned
+       ;; posts before any of the posts from hot. I *think* this is because of a
+       ;; bug in the reddit API, where pinned posts have a `false` :pinned
+       ;; field, and this is a workaround. This isn't explicitly documented
+       ;; anywhere on creddit's github page, but it is behavior I have observed.
+       ;; This is why we drop any "extra" posts in the cond above.
