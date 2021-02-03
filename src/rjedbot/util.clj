5fe22f56448cc740a4a1064c25b6882833589b49
Luciano Laratelli <luciano@laratelli.com>
2021-02-03 13:50:59 -0500

    remove unused import

--- src/rjedbot/util.clj
+++ src/rjedbot/util.clj
@@ -1,10 +1,11 @@
 (ns rjedbot.util
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io]))
 
 ;; Writes data to an edn file
 ;; thanks kiran
 ;; https://github.com/kiranshila/oofbot/blob/master/src/oofbot/core.clj
+
+
 (defn write-edn [data filename]
   (with-open [f (io/writer filename)]
     (binding [*out* f]
