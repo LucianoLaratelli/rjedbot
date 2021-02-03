55a1ed1b5ec07ca122cba669ea5dcf6ad9249b51
Luciano Laratelli <luciano@laratelli.com>
2021-02-03 16:49:48 -0500

    new configuration options

--- src/rjedbot/config.clj
+++ src/rjedbot/config.clj
@@ -1,15 +1,19 @@
 (ns rjedbot.config
-  (:require
-   [cprop.core :refer [load-config]]
-   [clojure.java.io :as io]))
+  "This namespace deals with reading from and writing to rjedbot's internal configuration file."
+  (:require [clojure.java.io :as io]))
 
 (def config (atom (read-string (slurp (io/resource "config.edn")))))
 
 (def default-max-posts 6)
 
 (def default-guild-data
-  {:max-posts 6})
+  {:max-posts 6
+   :favorites []})
 
 (defn known-guild?
   [guild-id]
   (contains? @config guild-id))
+
+(defn get-favorite-subreddit
+  [guild-id]
+  (rand-nth (:favorites (get @config guild-id))))
