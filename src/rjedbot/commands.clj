05eccd6754588db1d05e0bc91e6c719e0b5717ae
Luciano Laratelli <luciano@laratelli.com>
2021-02-04 22:18:11 -0500

    huge commit
    I did a lot of cleaning up of `command-handler` by abstracting things
    out into functions, making clear the comments, and renaming some
    variables. I also renamed a few required modules.

--- src/rjedbot/commands.clj
+++ src/rjedbot/commands.clj
@@ -1,14 +1,15 @@
 (ns rjedbot.commands
-  (:require [clojure.core.async :as a]
-            [clojure.pprint :as pp]
+  (:require [clojure.pprint :as pp]
             [clojure.string :as s]
             [jsonista.core :as j]
             [org.httpkit.server :as h]
-            [rjedbot.config :as config]
+            [rjedbot.config :as conf]
+            [rjedbot.guilds :as g]
             [rjedbot.log :refer [log]]
             [rjedbot.reddit :as reddit]
             [rjedbot.responses :as r]
-            [rjedbot.util :as u]))
+            [rjedbot.util :as util]
+            [clojure.core.async :as a]))
 
 (defn post-handler
   "Handle post URLs according to their extensions."
@@ -40,9 +41,9 @@
                                   :else {:raw post})))
                             posts)
         total (count posts)
-        skipped (count (u/get-inner-values-matching-key labelled-posts :skip))
-        embeddable (u/get-inner-values-matching-key labelled-posts :embed)
-        to-raw (u/get-inner-values-matching-key labelled-posts :raw)
+        skipped (count (util/get-inner-values-matching-key labelled-posts :skip))
+        embeddable (util/get-inner-values-matching-key labelled-posts :embed)
+        to-raw (util/get-inner-values-matching-key labelled-posts :raw)
         results-chan (a/chan)]
     (when (> skipped 0)
       (r/handle-rate-limited-call r/POST-string token
@@ -55,14 +56,10 @@
   "Do we have a valid amount of posts?"
   [c guild]
   ;; Allowing nil values as true because most of the commands do not actually
-  ;; include a post count -- only the /posts command does. [c guild]
+  ;; include a post count
   (if (nil? c)
     true
-    (<= 1 c (:max-posts (get @config/config guild)))))
-
-(defn get-surprises
-  [amount guild-id]
-  (set (repeatedly amount #(config/get-favorite-subreddit guild-id))))
+    (<= 1 c (:max-posts (get @conf/config guild)))))
 
 (defn command-handler
   "Determine what response is made to incoming commands."
@@ -72,40 +69,54 @@
             :status 200
             :body (j/write-value-as-string {:type 5})}
            true)
+  ;; The (= "post") mess is to deal with the fact that the commands that
+  ;; actually retrieve reddit commands are implemented as subcommands:
+  ;; https://discord.com/developers/docs/interactions/slash-commands#subcommands-and-subcommand-groups
+  ;; when the interaction requested by the user isn't in the `post` subcommand
+  ;; group, any options for that command are stored in the first level
+  ;; of "options" in the request body. however, when we have a `post`, the
+  ;; options rjedbot actually wants to pass to the reddit functions are stored
+  ;; in a second level of options. this pops up twice; once when we go get
+  ;; command options, and another time when we get the value of the first
+  ;; option.
   (let [data (get body "data")
-        command-options (first (get data "options"))
+        raw-name (get data "name")
+        command-options (if (= "post" raw-name) (first (get data "options")) (get data "options"))
         subcommand-options (get command-options "options")
-        command-name (get command-options "name")
+        command-name (if (= "post" raw-name)
+                       (get-in body ["data" "name"])
+                       raw-name)
         token (get body "token")
         guild (get body "guild_id")
-        subreddit (u/get-value-from-ith-map subcommand-options 0 "value")
-        post-type (keyword (u/get-value-from-ith-map subcommand-options 1 "value"))
-        post-time-scope (keyword (u/get-value-from-ith-map subcommand-options 2 "value"))
-        post-count (u/get-value-from-ith-map subcommand-options 3 "value")
-        arguments (filter #(not (nil? %)) [subreddit post-type post-time-scope post-count])]
+        subreddit (let [func #(util/get-value-from-ith-map % 0 "value")]
+                    (if (= "post" raw-name)
+                      (func subcommand-options)
+                      (func command-options)))
+        post-type (keyword (util/get-value-from-ith-map subcommand-options 1 "value"))
+        post-time-scope (keyword (util/get-value-from-ith-map subcommand-options 2 "value"))
+        post-count (util/get-value-from-ith-map subcommand-options 3 "value")
+        arguments (filter #(not (nil? %)) [subreddit post-type post-time-scope post-count])
+        chan (a/chan)]
 
-    (when (not (config/known-guild? guild))
-      (reset! config/config (assoc @config/config guild config/default-guild-data))
-      (u/write-to-resource "config.edn" (str @config/config)))
-    (log (str "serving request for subreddit " subreddit " at " (new java.util.Date)))
+    (when (not (conf/known-guild? guild))
+      (reset! conf/config (assoc @conf/config guild conf/default-guild-data))
+      (util/write-to-resource "config.edn" (str @conf/config)))
+    (log (str "serving request at " (new java.util.Date)))
     (if (valid-post-count? post-count guild)
       (try  (case command-name
-              "lofi" (r/POST-string token "p!play https://www.youtube.com/watch?v=5qap5aO4i9A")
-              ;;note that `subreddit` is used as the number of posts here -- the first argument to the 'surprise' command
-              "surprise" (do
-                           (log (str "handling " subreddit " surprises at " (new java.util.Date)))
-                           (post-handler (flatten (map #(reddit/get-posts %) (get-surprises (if (nil? subreddit) 1 subreddit) guild))) token))
-              ;; "update-max" (do
-              ;;           ;;"subreddit" used loosely here -- for most commands, the
-              ;;           ;;value of the 0th map will be a subreddit. Here
-              ;;           ;;subreddit is the new number of maximum posts a server
-              ;;           ;;owner wants to allow at a time.
-              ;;                (swap! max-posts subreddit)
-              ;;                (u/write-edn {:max-posts @max-posts} "config.edn"))
+              "lofi" (r/POST-string token "p!play https://www.youtube.com/watch?v=5qap5aO4i9A" chan)
+              "list-favorites" (g/send-favorites guild token chan)
+              "remove-favorite" (g/remove-favorite guild subreddit token chan)
+              "add-favorite" (g/add-favorite guild subreddit token chan)
+              ;;for most commands, the value of the 0th map will be a subreddit
+              ;;name. For update-max, subreddit is the new maximum number of
+              ;;posts. For surprises, it is the number of surprises to request.
+              "update-max" (conf/update-maximum guild token subreddit chan)
+              "surprise" (post-handler (conf/get-surprises subreddit guild token chan) token)
               ;; handle these cases: "hot" "category" "time" "many"
               (post-handler (apply reddit/get-posts arguments) token))
             (catch Exception e
               (log "Exception in command handler:")
               (log (.getMessage e))
-              (r/POST-string token (str "You asked for subreddit " subreddit ", which doesn't seem to exist."))))
-      (r/POST-string token (str "you asked for too many posts. max is " (:max-posts (get @config/config guild)))))))
+              (r/POST-string token (str "You asked for subreddit " subreddit ", which doesn't seem to exist.") chan)))
+      (r/POST-string token (str "you asked for too many posts. max is " (:max-posts (get @conf/config guild))) chan))))
