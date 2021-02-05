e462e15bc254893cdaebc640d2fe725fffdbc84f
Luciano Laratelli <luciano@laratelli.com>
2021-02-05 12:13:27 -0500

    move functions to config namespace
    makes more sense to me; I may eventually get rid of the guild namespace
    but for now this is a better way to sort things.

--- src/rjedbot/config.clj
+++ src/rjedbot/config.clj
@@ -1,6 +1,7 @@
 (ns rjedbot.config
   "This namespace deals with reading from and writing to rjedbot's internal configuration file."
   (:require [clojure.java.io :as io]
+            [clojure.string :as s]
             [rjedbot.config :as conf]
             [rjedbot.log :refer [log]]
             [rjedbot.responses :as r]
@@ -48,3 +49,49 @@
                              " favorites, so you're going to receive fewer posts than you requested.")]
             (r/handle-rate-limited-call r/POST-string token message chan)))
         (flatten (map #(reddit/get-posts %) (set (repeatedly amount #(conf/get-favorite-subreddit guild-id)))))))))
+
+(defn send-favorites
+  [guild token chan]
+  (let [favorites (:favorites (get @conf/config guild))
+        num_favs (count favorites)]
+    (if (> num_favs 0)
+      (let
+       [message "These are the favorite subreddits for  your server:\n"
+        message (str message (s/join "\n" favorites))]
+        (r/handle-rate-limited-call r/POST-string token message chan))
+      (r/handle-rate-limited-call r/POST-string token "There are no server favorites. Use `/add-favorite` to add some!" chan))))
+
+(defn remove-member
+  [s member]
+  (into #{} (filterv #(not (= member %)) s)))
+
+(defn remove-favorite
+  [guild favorite token chan]
+  (let [favorites (:favorites (get @conf/config guild))
+        num_favs (count favorites)]
+    (if (> num_favs 0)
+      (let [in [guild :favorites]
+            favorite (s/lower-case (s/trim favorite))
+            favs (get-in @conf/config in)]
+        (if (contains? favs favorite)
+          (do
+            (swap! conf/config #(assoc-in % in (remove-member (get-in % in) favorite)))
+            (u/write-to-resource "config.edn" (.toString @conf/config))
+            (r/handle-rate-limited-call r/POST-string token
+                                        (str "Removed " favorite " from server favorites list.") chan))
+          (r/handle-rate-limited-call r/POST-string token "That subreddit is not in the favorites list." chan)))
+      (r/handle-rate-limited-call r/POST-string token "There are no server favorites. Use `/add-favorite` to add some!" chan))))
+
+(defn add-favorite
+  "Add a subreddit to the favorites list."
+  [guild favorite token chan]
+  (let [in [guild :favorites]
+        favs (get-in @conf/config in)]
+    (if (contains? favs favorite)
+      (r/handle-rate-limited-call r/POST-string token "That subreddit is already in the favorites list." chan)
+      (do
+        (swap! conf/config #(assoc-in % in (conj (get-in % in) (s/lower-case favorite))))
+        (u/write-to-resource "config.edn" (.toString @conf/config))
+        (r/handle-rate-limited-call r/POST-string token
+                                    (str "Added " favorite " to server favorites list. Favorites list is now:\n"
+                                         (s/join "\n" (:favorites (get @conf/config guild)))) chan)))))
